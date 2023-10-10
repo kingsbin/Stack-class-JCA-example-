3bf0e59f9d2e9987edbc987f1cb9e749f1c1b911
jcampbel2-kings <james.campbell@kingsacademies.uk>
2023-10-10 18:49:39 +0100

    added initial outline methods

--- Classes.cs
+++ Classes.cs
@@ -3,6 +3,42 @@ using System.Data;
 namespace StackQueueExample
 {
     class MyQueue {
+        bool isEmpty;
+        bool isFull;
+        int maxSize;
+        string[] queue;
+        int front;
+        int rear;
+
+        public bool IsEmpty {get {return isEmpty;}}
+        public bool IsFull {get {return isFull;}}  
+
+        public MyQueue(int m){
+            maxSize=m;
+            queue=new string[maxSize];
+            front=-1;
+            rear=-1;
+            isEmpty=true;
+            isFull=false;
+        }  
+
+        public void EnQueue(string item) {
+            if (isEmpty){
+                front=0;
+                isEmpty=false;
+                rear=0;
+            } else {
+                rear++;
+            }
+            queue[rear] = item;
+
+        }
+
+        public string DeQueue(){
+            string rval= queue[front];
+            front++;
+            return rval;
+        }
 
     }
     class MyStack{
@@ -25,7 +61,7 @@ namespace StackQueueExample
         public string Pop(){
             string rval;
             if (isEmpty) {
-                rval= "Error - Stack Underflow";
+                rval= "Er'ror - Stack Underflow";
             } else {
                 rval=stack[head];
                 stack[head]="";
